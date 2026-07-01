f4e3cc77c416bf588e92e749b51964dc8cda683a
contatoglauciaoliveira-wq <contatoglauciaoliveira@gmail.com>
2026-07-01 18:50:18 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../glasare-completo';
+import App from './glasare-completo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
